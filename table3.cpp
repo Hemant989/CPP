702e8f9d5d3ad781e7bbe2dabe7f1bd605e7a08c
Hemant Patidar <112345438+Hemant989@users.noreply.github.com>
2023-09-25 11:43:47 +0530

    table 3

---
+++ table3.cpp
@@ -0,0 +1,18 @@
+//print table of n number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cout<<"Enter Number :";
+    cin>>n;
+
+        for(i=1;i<=10;i++)
+        {
+          cout <<n<<"*"<<i<<" = "<<i*n<<endl;
+        }
+
+
+}
